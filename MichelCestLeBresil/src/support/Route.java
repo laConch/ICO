a47738f3d7fc256ebeb1be70521fe81793615b36
Edouard Mertz <edouardmertz@Edouards-MacBook-Pro.local>
2020-03-14 16:05:46 +0100

    ajout de l'algo recuit simule et du fichier csv worldcities

--- MichelCestLeBresil/src/support/Route.java
+++ MichelCestLeBresil/src/support/Route.java
@@ -7,26 +7,30 @@ import support.City;
 
 public class Route {
 	private ArrayList<City> cities = new ArrayList<City>();
+	private City startCity;
 
 	public String toString() {
 		return Arrays.toString(cities.toArray());
 	}
 
 	//constructeurs
-	//-->� partir d'une autre route
-	public Route(Route route) {
-		/*
-		 * ajouter les villes de la route en param comme villes de cette route
-		 */
-		//� compl�ter
-		
-		
+	public Route(Route route) {	//-->à partir d'une autre route
+		this.startCity = route.getStartCity();
+		for(int i = 0; i < route.getCities().size(); i++) {
+			this.cities.add(route.getCities().get(i));
+		}
 	}
-
-	//-->� partir d'une liste de villes qu'on m�lange pour ne pas garder le m�me ordre
-	public Route(ArrayList<City> cities) {
-		this.cities.addAll(cities);
-		//m�langer al�atoirement les villes de la route
+	
+	public Route(ArrayList<City> cities, int indexStartCity) { //-->à partir d'une liste de villes qu'on mélange pour ne pas garder le même ordre
+		for(int i = 0; i < cities.size(); i++) {
+			if(i == indexStartCity) {
+				this.startCity = cities.get(i);
+			}
+			else {
+				this.cities.add(cities.get(i));
+			}
+		}
+		//mélanger aléatoirement les villes de la route
 		Collections.shuffle(this.cities);
 	}
 
@@ -34,15 +38,30 @@ public class Route {
 	public ArrayList<City> getCities() {
 		return cities;
 	}
+	public City getStartCity() {
+		return startCity;
+	}
 
 	public double getTotalDistance() {
-		int citiesSize = this.cities.size();
+		int citiesSize = cities.size();
 		double totalDistance = 0;
 		
 		for(int i = 0; i < citiesSize - 1; i++){
 			totalDistance += cities.get(i).measureDistance(cities.get(i+1));
 		}
-		totalDistance += cities.get(citiesSize-1).measureDistance(cities.get(0));
+		totalDistance += startCity.measureDistance(cities.get(0));
+		totalDistance += startCity.measureDistance(cities.get(citiesSize-1));
+		
 		return totalDistance;
+	}
+	
+	public void printCitiesNameOfRoute() {
+		int citiesSize = cities.size();
+		System.out.println("Listes des villes parcourues dans l'ordre :");
+		System.out.print(startCity.getName() + ", ");
+		for(int i = 0; i < citiesSize; i++){
+			System.out.print(cities.get(i) + ", ");
+		}
+		System.out.print(startCity.getName());
 	}
 }
